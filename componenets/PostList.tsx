7bd50e28aea4b6bdfbb0cdaf2ce3a44c0ae44a94
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-02-12 14:55:02 +0100

    Add link to individual post in PostList component for improved navigation

--- componenets/PostList.tsx
+++ componenets/PostList.tsx
@@ -1,4 +1,5 @@
 import { Post } from '@/types/Types';
+import Link from 'next/link';
 import { capitalizeTitle, limitText } from '@/lib/helpers';
 
 export default function PostList({ posts }: { posts: Post[] }) {
@@ -10,11 +11,11 @@ export default function PostList({ posts }: { posts: Post[] }) {
 
         return (
           <div key={post.id} className='border border-black'>
-            <div className='border-b my-4'>
+            <Link href={`/posts/${post.id}`}>
               <h2 className='text-2xl font-bold border-b border-white my-2'>
                 {title}
               </h2>
-            </div>
+            </Link>
             <p>{body}</p>
           </div>
         );
