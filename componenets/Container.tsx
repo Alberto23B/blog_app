03fc33c9c367149ef1d8dd56621192d45f2b5a13
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-02-12 14:20:15 +0100

    Add modularity to the layout

---
+++ componenets/Container.tsx
@@ -0,0 +1,5 @@
+export default function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='flex items-center justify-center  py-20'>{children}</div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
 import { GetStaticProps } from 'next';
-import Navbar from '@/componenets/Navbar';
 import PostList from '@/componenets/PostList';
 import { Post } from '@/types/Types';
+import Layout from '@/componenets/Layout';
+import Container from '@/componenets/Container';
 
 export default function Home({ posts }: { posts: Post[] }) {
   return (
     <>
-      <Navbar />
-      <main className='flex items-center justify-center min-h-screen py-20'>
-        <div className='sm:block hidden border w-1/4 flex-shrink border-white'>
-          <p>side 1</p>
-        </div>
-        <div className='grid grid-cols-1 gap-3 w-1/2 min-w-96 '>
-          <PostList posts={posts} />
-        </div>
-        <div className='flex-shrink w-1/4 sm:block hidden border border-white'>
-          <p>side 2</p>
-        </div>
-      </main>
+      <Layout>
+        <Container>
+          <div className='sm:block hidden border w-1/4 flex-shrink border-white'>
+            <p>side 1</p>
+          </div>
+          <div className='grid grid-cols-1 gap-3 w-1/2 min-w-96 '>
+            <PostList posts={posts} />
+          </div>
+          <div className='flex-shrink w-1/4 sm:block hidden border border-white'>
+            <p>side 2</p>
+          </div>
+        </Container>
+      </Layout>
     </>
   );
 }
