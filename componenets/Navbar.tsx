33fb9ae6b80d1f778b5278567d84e452270cad35
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-02-16 18:32:35 +0100

    Update Navbar to use full width and add scrollbar-gutter for improved layout stability

--- componenets/Navbar.tsx
+++ componenets/Navbar.tsx
@@ -3,7 +3,7 @@ import HomeButton from './HomeButton';
 
 export default function Navbar() {
   return (
-    <nav className='flex fixed w-full items-center justify-center h-10 bord z-50'>
+    <nav className='flex fixed w-screen items-center justify-center h-10 bord z-50'>
       <div className='flex w-xl min-w-96 justify-center'>
         <HomeButton />
         <AboutButton />
