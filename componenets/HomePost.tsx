4c2cebf3929f181c606c2c0a74e13d81cf346940
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-02-17 08:49:48 +0100

    Enhance HomePost component with text limiting and responsive body display

--- componenets/HomePost.tsx
+++ componenets/HomePost.tsx
@@ -1,12 +1,13 @@
 import { Post } from '@/types/Types';
 import HashGrid from './HashGrid';
-import { capitalizeTitle, generateHashtags } from '@/lib/helpers';
+import { capitalizeTitle, limitText, generateHashtags } from '@/lib/helpers';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function HomePost({ post }: { post: Post }) {
   const title = capitalizeTitle(post.title);
   const hashtags = generateHashtags(post.title);
+  const body = limitText(post.body, 100);
 
   const router = useRouter();
   const { hashtag } = router.query;
@@ -22,15 +23,16 @@ export default function HomePost({ post }: { post: Post }) {
   return (
     <>
       {post && (
-        <div className='col-span-2  mx-auto h-fit bord rounded-br-xl'>
+        <div className='col-span-2 mx-auto h-fit bord rounded-br-xl'>
           <p className='float-end w-1/5 block bg-cyan-800 pr-4 text-end'>
             Latest
           </p>
           <Link href={`/posts/${post.id}`}>
             <h2 className='text-4xl px-4 py-2 title'>{title}</h2>
           </Link>
-          <div className='p-4 py-10'>
-            <p className='text-2xl'>{post.body}</p>
+          <div className='p-4'>
+            <p className='sm:text-2xl hidden sm:block'>{post.body}</p>
+            <p className='sm:text-2xl block sm:hidden'>{body}</p>
           </div>
           <HashGrid
             hashtags={hashtags}
--- componenets/PostsContainer.jsx
+++ componenets/PostsContainer.jsx
@@ -5,7 +5,7 @@ export default function PostContainer({ mainPost, posts }) {
   return (
     <div className='grid grid-cols-1 gap-4 px-5 md:px-0 md:w-3/5 pt-6 min-w-96 h-[calc(100vh-2.5rem)]'>
       <HomePost post={mainPost} />
-      <div className='grid grid-cols-2 gap-4 overflow-scroll no-scrollbar'>
+      <div className='grid sm:grid-cols-2 gap-4 overflow-scroll no-scrollbar'>
         <PostList posts={posts} />
       </div>
     </div>
