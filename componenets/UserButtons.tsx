a7d49d39a56546554e69cff96ed2e99c30443d7b
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-02-18 16:18:14 +0100

    Apply smoothing to buttons

--- componenets/UserButtons.tsx
+++ componenets/UserButtons.tsx
@@ -1,10 +1,10 @@
 export default function UserButtons() {
   return (
     <div className='gap-2 text-center'>
-      <button className='rounded-xl bg-[#393737] 3xl:text-3xl text-xl mx-2 w-fit px-6 inline-block'>
+      <button className='rounded-xl bg-[#393737] border-[#4a4848] border 3xl:text-3xl text-xl mx-2 w-fit px-6 inline-block smoothing'>
         Login
       </button>
-      <button className='rounded-xl bg-[#393737] 3xl:text-3xl text-xl mx-2 w-fit px-6 inline-block'>
+      <button className='rounded-xl bg-[#393737] border-[#4a4848] border 3xl:text-3xl text-xl mx-2 w-fit px-6 inline-block smoothing'>
         Sign In
       </button>
     </div>
