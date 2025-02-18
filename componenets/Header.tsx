b2336210aa5892863f7a692db362d2ce1577b932
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-02-18 10:17:02 +0100

    Make mainpost animation desktop only

--- componenets/Header.tsx
+++ componenets/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header({ children }: { children: React.ReactNode }) {
   return (
     <header className='col-span-3'>
-      <h1 className='sm:text-7xl text-4xl font-bold my-4 tracking-tight leading-14 sm:animate-lineHeightExpand'>
+      <h1 className='sm:text-7xl text-4xl font-bold my-4 tracking-tight animate-lineHeightExpand'>
         {children}
       </h1>
     </header>
